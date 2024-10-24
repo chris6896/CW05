cb9f0e030373b255c4c7557afc6cddfc46d52c3a
chris6896 <159868610+chris6896@users.noreply.github.com>
2024-10-23 23:38:02 -0400

    bug fix in databasehelper

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -1,31 +1,34 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
-
 class DatabaseHelper {
   static final _databaseName = "aquarium.db";
-  static final _databaseVersion = 2;
+  static final _databaseVersion = 2; 
 
   static final tableSettings = 'settings';
-  static final columnId = 'id';
-  static final columnName = 'name';
+
+  static final columnId = 'id'; 
+  static final columnName = 'name'; 
   static final columnColor = 'color';
   static final columnSpeed = 'speed';
   static final columnFishCount = 'fish_count';
 
   DatabaseHelper._privateConstructor();
   static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
   static Database? _database;
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDatabase();
     return _database!;
   }
-    _initDatabase() async {
+
+  _initDatabase() async {
     String path = join(await getDatabasesPath(), _databaseName);
     return await openDatabase(path, version: _databaseVersion, onCreate: _onCreate);
   }
-    Future _onCreate(Database db, int version) async {
+
+  Future _onCreate(Database db, int version) async {
     await db.execute('''
       CREATE TABLE $tableSettings (
         $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -36,7 +39,8 @@ class DatabaseHelper {
       )
     ''');
   }
-    Future<int> saveSettings(String name, int color, double speed, int fishCount) async {
+
+  Future<int> saveSettings(String name, int color, double speed, int fishCount) async {
     Database db = await instance.database;
 
     return await db.insert(tableSettings, {
@@ -62,10 +66,8 @@ class DatabaseHelper {
 
     if (result.isNotEmpty) {
       return result.first;
-    }else {
+    } else {
       return null;
     }
-
   }
-
 }
